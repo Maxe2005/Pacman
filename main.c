5a87e341d70c43603c92b3532ff6ae55b3ee22a5
Maxence CHOSIEL <maxencechoisel@gmail.com>
2025-01-15 15:08:10 +0100

    ajout joueur_pacman

--- main.c
+++ main.c
@@ -3,6 +3,13 @@
 #include <stdlib.h>
 #include <time.h>
 
+struct Joueur_pacman
+{
+    int position_x;
+    int position_y;
+};
+typedef struct Joueur_pacman pacman;
+
 
 int main (int argc, char *argv[]){
 }
