26d222630d8770d1dca057b1be6b6898cf10afb8
Maxence CHOSIEL <maxencechoisel@gmail.com>
2025-01-15 15:10:17 +0100

    ajout fantome

--- main.c
+++ main.c
@@ -3,13 +3,19 @@
 #include <stdlib.h>
 #include <time.h>
 
-struct Joueur_pacman
+struct Player_pacman
 {
     int position_x;
     int position_y;
 };
-typedef struct Joueur_pacman pacman;
+typedef struct Player_pacman pacman;
 
+struct PNJ_ghost
+{
+    int position_x;
+    int position_y;
+};
+typedef struct PNJ_ghost ghost;
 
 int main (int argc, char *argv[]){
 }
